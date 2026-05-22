2640b2beb2c005e86380ffbb37bc6b5aa55e2209
melvstein <melvinbayogo@gmail.com>
2026-05-22 23:06:36 +0800

    melvstein portfolio v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/assets/images/logos/melvstein_logo.png"
+          src="/assets/images/logos/melvstein_logo.svg"
           alt="MJ Logo"
           width={50}
           height={50}
