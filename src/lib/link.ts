aa07196ad3027579cb0523c5c7c977d71c3d1b93
melvin.php.ph <melvin.php.ph@tripleonetech.net>
2026-05-30 03:33:55 +0800

    melvstein portfolio v1

--- src/lib/link.ts
+++ src/lib/link.ts
@@ -1,7 +1,8 @@
 enum LINK {
     GITHUB = "https://github.com/melvstein",
-    LINKEDIN = "https://www.linkedin.com/in/melvin-justine-bayogo-836679194",
-    RESUME = "/resume/melvin-justine-bayogo.pdf",
+    LINKEDIN = "https://www.linkedin.com/in/melvstein/",
+    //LINKEDIN = "https://www.linkedin.com/in/melvin-justine-bayogo-836679194",
+    RESUME = "/resume/melvin-justine-bayogo-resume.pdf",
 }
 
 export default LINK;
